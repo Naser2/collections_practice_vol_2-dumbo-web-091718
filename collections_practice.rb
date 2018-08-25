cc26b57e170b81cdb1c216a11b461871d1c915a4
Nasser Sanou <nassersanou23@gmail.com>
2018-08-25 06:07:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,6 @@
 # # your code goes here
 def begins_with_r(array)
+  #"is" just a var to hold the conditional statement 
 	  is = true
 	  array.each do |el|
 	    is = false if el[0] != "r"
@@ -14,4 +15,12 @@ def contain_a(array)
 	  nu_ar << el unless !el.include?("a")
 	  end
 	  new_arr
+	end
+	
+	def first_wa(array)
+	  was_ar= []
+	  array.each do |el
+	  was_ar << el unless !el.include?("wa")
+	  end
+	  was_arr
 	end
