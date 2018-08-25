e47eb1de6db0c462f0e85721a914e9fc6d7be772
Nasser Sanou <nassersanou23@gmail.com>
2018-08-25 06:02:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
-# your code goes here
+# # your code goes here
+def begins_with_r(array)
+	  is = true
+	  array.each do |el|
+	    is = false if el[0] != "r"
+	  end
+	  is
+	end
+
+def contain_a(array)
+| #nu_ar dot push 'unless' it does-not contain 'a' or push 'if' it contains 'a'
+	  nu_ar= []
+	  array.each do |el
+	  nu_ar << el unless !el.include?("a")
+	  end
+	  new_arr
+	end
